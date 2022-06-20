c8402698a200a1584d7c6d7efb4905491955d264
thedevyansh <schawla333333@gmail.com>
2022-06-20 12:52:26 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
+import {
+  keypairIdentity,
+  Metaplex,
+  mockStorage,
+  useMetaplexFileFromBrowser,
+} from '@metaplex-foundation/js';
+import { Connection, clusterApiUrl, Keypair } from '@solana/web3.js';
 import './App.css';
+import kp from './keypair.json';
+import { Buffer } from 'buffer';
 
-function App() {
+window.Buffer = Buffer;
+const arr = Object.values(kp._keypair.secretKey);
+const secret = new Uint8Array(arr);
+const myKeyPair = Keypair.fromSecretKey(secret);
+
+export default function Home() {
+  // console.log(myKeyPair.publicKey.toString());
+  const inputRef = useRef();
+  const [metadataUri, setMetadataUri] = useState(null);
+
+  useEffect(() => {
+    const input = inputRef.current;
+
+    const OnChange = async event => {
+      const browserFiles = event.target.files;
+      const connection = new Connection(clusterApiUrl('devnet'));
+      const metaplex = Metaplex.make(connection)
+        .use(keypairIdentity(myKeyPair))
+        .use(mockStorage());
+
+      const { uri } = await metaplex.nfts().uploadMetadata({
+        name: 'My first ever NFT',
+        description: 'This is my first ever NFT',
+        image: await useMetaplexFileFromBrowser(browserFiles[0]),
+      });
+
+      setMetadataUri(uri);
+
+      const { nft } = await metaplex.nfts().create({
+        uri,
+      });
+
+      console.log(nft);
+
+      const [myNft] = await metaplex.nfts().findAllByMintList([nft.mint]);
+      console.log('MY NFTS', myNft);
+    };
+
+    input.addEventListener('change', OnChange);
+    return () => input.removeEventListener('change', OnChange);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input ref={inputRef} type='file' id='file-selector' multiple></input>
+      {metadataUri && (
+        <h3 className='metadata-uri'>
+          Metadata URI: {JSON.stringify(metadataUri)}
+        </h3>
+      )}
     </div>
   );
 }
-
-export default App;
