05a7157c4473595fdaa0f090d99e95440f4eb846
thedevyansh <schawla333333@gmail.com>
2022-06-21 11:58:25 +0530

    Revert "Delete android files"
    This reverts commit 3344f9ab792148e9ca0426a4ce394ed3b2c63869.

---
+++ app/src/main/java/com/devyansh/timefighter/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.devyansh.timefighter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    internal lateinit var tapMeButton: Button
+    internal lateinit var gameScoreTextView: TextView
+    internal lateinit var timeLeftTextView: TextView
+
+    internal var score = 0
+
+    internal var gameStarted = false
+
+    internal lateinit var countDownTimer: CountDownTimer
+    internal var initialCountDown: Long = 60000
+    internal var countDownInterval: Long = 1000
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate called. Score is $score")
+
+        tapMeButton = findViewById(R.id.tapMeButton)
+        gameScoreTextView = findViewById(R.id.gameScoreTextView)
+        timeLeftTextView = findViewById((R.id.timeLeftTextView))
+
+        tapMeButton.setOnClickListener { view ->
+            incrementScore()
+        }
+
+        resetGame()
+    }
+
+    private fun resetGame() {
+        score = 0
+
+        gameScoreTextView.text = getString(R.string.yourScore, score)
+
+        val initialTimeLeft = initialCountDown / 1000
+        timeLeftTextView.text = getString(R.string.timeLeft, initialTimeLeft)
+
+        countDownTimer = object : CountDownTimer(initialCountDown, countDownInterval) {
+            override fun onTick(millisUntilFinished: Long) {
+                val timeLeft = millisUntilFinished / 1000
+                timeLeftTextView.text = getString(R.string.timeLeft, timeLeft)
+            }
+
+            override fun onFinish() {
+                gameEnd()
+            }
+        }
+
+        gameStarted = false
+    }
+
+    private fun incrementScore() {
+        if(!gameStarted) {
+            gameStarted()
+        }
+
+        score += 1
+        val newScore = getString(R.string.yourScore, score)
+        gameScoreTextView.text = newScore
+    }
+
+    private fun gameStarted() {
+        countDownTimer.start()
+        gameStarted = true
+    }
+
+    private fun gameEnd() {
+        Toast.makeText(this, getString(R.string.gameOverMessage, score), Toast.LENGTH_LONG).show()
+        resetGame()
+    }
+}
